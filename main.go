7e83c612adbc7f7e0d7ab62b06ef76a60d1d782c
Michael <michaeljsaenz@gmail.com>
2022-05-04 23:53:51 -0500

    add structs/functions to call google trends and unmarshal rss data

--- main.go
+++ main.go
@@ -1,25 +1,76 @@
 package main
 
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+//unmarshal xml
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel *Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Title    string `xml:"title"`
+	ItemList []Item `xml:"item"`
+}
+
 type Item struct {
-	Title     string
-	Link      string
-	Traffic   string
-	NewsItems []News
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Traffic   string `xml:"approx_traffic"`
+	NewsItems []News `xml:"news_item"`
 }
 
 type News struct {
-	Headline     string
-	HeadlineLink string
+	Headline     string `xml:"news_item_title"`
+	HeadlineLink string `xml:"news_item_url"`
 }
 
 func main() {
-	readGoogleTrends
+	var r RSS
+	data := readGoogleTrends()
+
+	//unmarshal data(rss xml)
+	err := xml.Unmarshal(data, &r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\n Below are all the Google Search Trends For Today!")
+	fmt.Println("----------------------------------------------------")
+
+	for i := range r.Channel.ItemList {
+		rank := (i + 1)
+		fmt.Println("#", rank)
+		fmt.Println("Search Term: ", r.Channel.ItemList[i].Title)
+		fmt.Println("Link to the Trend: ", r.Channel.ItemList[i].Link)
+		fmt.Println("Heading: ", r.Channel.ItemList[i].NewsItems[0].Headline)
+		fmt.Println("Link to article:", r.Channel.ItemList[i].NewsItems[0].HeadlineLink)
+		fmt.Println("------------------------------------------------------------------")
+	}
+
 }
 
-func readGoogleTrends() {
-
+func getGoogleTrends() *http.Response {
+	response, err := http.Get("https://trends.google.com/trends/trendingsearches/daily/rss?geo=US")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return response
 }
 
-func getGoogleTrends() {
+func readGoogleTrends() []byte {
+	response := getGoogleTrends()
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return responseData
 
 }
